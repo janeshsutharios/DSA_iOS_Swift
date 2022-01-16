ba81a588ce09a5348cdd0970a368ea8191202173
Jack <janeshsuthar.ss@smartdatainc.io>
2022-01-16 08:14:17 +0530

    first commit

---
+++ DSA_iOS/DSA_iOS/Question1/Question1.swift
@@ -0,0 +1,11 @@
+//
+//  Question1.swift
+//  DSA_iOS
+//
+//  Created by Janesh Suthar on 16/01/22.
+//
+
+import Foundation
+// WAP to reverse a string in swift
+
+
---
+++ DSA_iOS/DSA_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DSA_iOS
+//
+//  Created by Janesh Suthar on 16/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
