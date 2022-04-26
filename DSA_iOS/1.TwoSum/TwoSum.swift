897da32bc2fc4198c560edfab41daecc82718a73
Jack <janeshsuthar.ss@smartdatainc.io>
2022-04-26 16:34:18 +0530

    Two sum
    Two sum

---
+++ DSA_iOS/1.TwoSum/TwoSum.swift
@@ -0,0 +1,51 @@
+// Leet code 1. Two sum https://leetcode.com/problems/two-sum/
+
+//  TwoSum.swift
+//  DSA_iOS
+//
+//  Created by Janesh Suthar on 26/04/22.
+//
+/**
+ Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+ You can return the answer in any order.
+
+  
+
+ Example 1:
+
+ Input: nums = [2,7,11,15], target = 9
+ Output: [0,1]
+ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ Example 2:
+
+ Input: nums = [3,2,4], target = 6
+ Output: [1,2]
+ Example 3:
+
+ Input: nums = [3,3], target = 6
+ Output: [0,1]
+  
+
+ Constraints:
+
+ 2 <= nums.length <= 104
+ -109 <= nums[i] <= 109
+ -109 <= target <= 109
+ Only one valid answer exists.
+ */
+import Foundation
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dictionary = [Int:Int]()
+       for (i, n) in nums.enumerated() {
+           if let last = dictionary[target - n] {
+               return [last, i]
+           }
+           dictionary[n] = i
+       }
+       return []
+    }
+}
