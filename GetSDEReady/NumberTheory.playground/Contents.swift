7a7189d14a68eb3284f19a14105e72460fe96253
Janesh suthar <janesh.stack@gmail.com>
2025-05-11 19:48:49 +0530

    added 2427. Number of Common Factors

--- GetSDEReady/NumberTheory.playground/Contents.swift
+++ GetSDEReady/NumberTheory.playground/Contents.swift
@@ -1,3 +1,43 @@
-import UIKit
+import Fondation
+//https://leetcode.com/problems/number-of-common-factors/description/
+// TC:O(log(min(a, b)) + √g), where g = gcd(a, b)
+// SC: O(1)
 
-var greeting = "Hello, playground"
+class Solution {
+    func commonFactors(_ a: Int, _ b: Int) -> Int {
+        let gcd = gcd(a,b)
+        return commonDivisior(gcd)
+    }
+    
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        var a = a
+        var b = b
+        while b != 0 {
+            let temp = b
+            b = a % b
+            a = temp
+        }
+        return a
+    }
+    
+    func commonDivisior(_ n: Int) -> Int {
+        let sqrt = Int(Double(n).squareRoot())
+        var count = 0
+        for i in 1...sqrt {
+            if n % i == 0 {
+                if n / i == i {
+                    // only one divider i.e. 36/6 == 6
+                    count += 1 // i == n/i → count only once (perfect square root)
+                } else {
+                    // example two divider i.e. 1 and 36
+                    count += 2// i and n/i are distinct → count both
+                }
+            }
+        }
+        return count
+    }
+}
+/*
+example 625 125 
+36 72 
+*/
