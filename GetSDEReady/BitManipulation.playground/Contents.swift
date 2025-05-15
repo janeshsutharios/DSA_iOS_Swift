28f242a1ebaacd161a5e78d882b135b0a2813954
Janesh suthar <janesh.stack@gmail.com>
2025-05-15 21:55:54 +0530

    prime-number-of-set-bits-in-binary-representation

--- GetSDEReady/BitManipulation.playground/Contents.swift
+++ GetSDEReady/BitManipulation.playground/Contents.swift
@@ -25,3 +25,44 @@ class Solution {
         return xorNumber
     }
 }
+//https://leetcode.com/problems/prime-number-of-set-bits-in-binary-representation/description/
+
+class Solution {
+    
+    // Helper function to check if a number is prime
+ private func isPrime(_ num: Int) -> Bool {
+    if num <= 1 { return false }
+    if num <= 3 { return true }
+    let limit = Int(Double(num).squareRoot())
+    for i in 2...limit {
+        if num % i == 0 {
+            return false
+        }
+    }
+    return true
+}
+
+    // Main function to count numbers in [left, right] whose set bit count is prime
+    func countPrimeSetBits(_ left: Int, _ right: Int) -> Int {
+        var primeSetBitCount = 0
+        
+        for number in left...right {
+            // Count set bits in the binary representation of the number
+            var count = 0
+            var num = number
+            while num > 0 {
+                if num % 2 == 1 {
+                    count += 1
+                }
+                num /= 2
+            }
+            
+            // If count of set bits is a prime number, increment result
+            if isPrime(count) {
+                primeSetBitCount += 1
+            }
+        }
+        
+        return primeSetBitCount
+    }
+}
