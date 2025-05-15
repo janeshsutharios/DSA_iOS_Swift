e63ac8c854768de23534f14e759c19d87ee4052c
Janesh suthar <janesh.stack@gmail.com>
2025-05-15 20:33:33 +0530

    single number

--- GetSDEReady/BitManipulation.playground/Contents.swift
+++ GetSDEReady/BitManipulation.playground/Contents.swift
@@ -1,3 +1,15 @@
-import UIKit
+//https://leetcode.com/problems/single-number/
+// [2,2,1]  output 1
+// [4,1,2,1,2] output 4
+class Solution {
+    func singleNumber(_ nums: [Int]) -> Int {
+        guard !nums.isEmpty else { return 0 }
+        
+        var result = 0 // because 0 with any ^ will give that real number 0 ^ 3 = 3
 
-var greeting = "Hello, playground"
+        for num in nums {
+            result ^= num
+        }
+        return result
+    }
+}
