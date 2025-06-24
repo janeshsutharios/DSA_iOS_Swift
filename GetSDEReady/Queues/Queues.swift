2e2717a5752234eb988de3763cc26911033dfbf9
Janesh suthar <janesh.stack@gmail.com>
2025-06-24 15:57:50 +0530

    Update Queues.swift

--- GetSDEReady/Queues/Queues.swift
+++ GetSDEReady/Queues/Queues.swift
@@ -20,3 +20,26 @@ class Solution {
         return -1
     }
 }
+// https://getsdeready.com/courses/dsa/lesson/time-needed-to-buy-tickets/
+// https://leetcode.com/problems/time-needed-to-buy-tickets/
+class Solution {
+    func timeRequiredToBuy(_ tickets: [Int], _ k: Int) -> Int {
+        var time = 0                         // Total time counter
+        let target = tickets[k]             // Number of tickets person at index k needs
+
+        for i in 0..<tickets.count {
+            if i <= k {
+                // If the person is before or exactly at position k,
+                // they get served in all `target` rounds (or less if they have fewer tickets)
+                time += min(tickets[i], target)
+            } else {
+                // If the person is after k,
+                // they only get served in the first `target - 1` rounds because
+                // person k leaves the queue after their last ticket
+                time += min(tickets[i], target - 1)
+            }
+        }
+
+        return time                         // Return the total time taken until person k is done
+    }
+}
