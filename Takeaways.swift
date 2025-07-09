9d915d79f36f38c562d37b23e711e69eab281db3
Janesh suthar <janesh.stack@gmail.com>
2025-07-09 08:06:06 +0530

    Update and rename Takeaways.md to Takeaways.swift

--- Takeaways.swift
+++ Takeaways.swift
@@ -2,3 +2,7 @@
 #2 Int to Char conversion - let char = Character(UnicodeScalar(i + Int(Character("a").asciiValue!))!)
 #3 Char to Int conversion - let index = Int(char.asciiValue! - Character("a").asciiValue!)
 
+// MARK: How to access string index
+let name = "Janesh"
+print("second char", name[name.index(name.startIndex, offsetBy: 1)])
+print("third last char", name[name.index(name.endIndex, offsetBy: -3)])
