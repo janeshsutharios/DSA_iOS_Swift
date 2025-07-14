d8878c8b296d67b31407d2e4de76220a7ebdf148
Janesh suthar <janesh.stack@gmail.com>
2025-07-14 14:09:46 +0530

    Update DynamicProgrammingBeginner.swift

--- GetSDEReady/DynamicProgramming(Beginner)/DynamicProgrammingBeginner.swift
+++ GetSDEReady/DynamicProgramming(Beginner)/DynamicProgrammingBeginner.swift
@@ -106,3 +106,58 @@ class Solution {
     return prev1
     }
 }
+
+// https://getsdeready.com/courses/dsa/lesson/house-robber/
+// https://leetcode.com/problems/house-robber/description/
+class Solution {
+    func rob(_ houses: [Int]) -> Int {
+        guard !houses.isEmpty else { return 0 }
+
+        var maxLootWithPreviousHouse = 0   // equivalent to prev1
+        var maxLootSkippingPrevious = 0    // equivalent to prev2
+
+        for houseValue in houses {
+            let temp = maxLootWithPreviousHouse
+
+            // Choose max between:
+            // 1. Skipping this house (maxLootWithPreviousHouse)
+            // 2. Robbing this house and skipping the previous (maxLootSkippingPrevious + houseValue)
+            maxLootWithPreviousHouse = max(maxLootSkippingPrevious + houseValue, maxLootWithPreviousHouse)
+
+            maxLootSkippingPrevious = temp
+        }
+
+        return maxLootWithPreviousHouse
+    }
+}
+
+    /*func rob(_ nums: [Int]) -> Int {
+        // Edge case: If there are no houses, return 0
+        if nums.isEmpty {
+            return 0
+        }
+        
+        // Edge case: If there is only one house, return the amount in that house
+        if nums.count == 1 {
+            return nums[0]
+        }
+        
+        // Create an array to store the maximum amount of money that can be robbed up to each house
+        var dp = [Int](repeating: 0, count: nums.count)
+        
+        // Base cases:
+        dp[0] = nums[0] // If there's only one house, the max amount is the amount in that house
+        dp[1] = max(nums[0], nums[1]) // If there are two houses, take the maximum of the two
+        
+        // Iterate through the houses starting from the third house
+        for i in 2..<nums.count {
+            // For each house, choose between robbing this house and adding its value to the maximum amount up to house i-2
+            // or not robbing this house and taking the maximum amount up to house i-1
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        }
+        
+        // The last element in the dp array contains the maximum amount that can be robbed from all houses
+        return dp[nums.count - 1]
+    }
+}*/
+
