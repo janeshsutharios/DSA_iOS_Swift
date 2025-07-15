4fb0c59bfa0fed2332039fde36eb035b174a7f6d
Janesh suthar <janesh.stack@gmail.com>
2025-07-15 10:03:21 +0530

    Update DynamicProgrammingBeginner.swift

--- GetSDEReady/DynamicProgramming(Beginner)/DynamicProgrammingBeginner.swift
+++ GetSDEReady/DynamicProgramming(Beginner)/DynamicProgrammingBeginner.swift
@@ -161,3 +161,42 @@ class Solution {
     }
 }*/
 
+// https://getsdeready.com/courses/dsa/lesson/climbing-stairs/
+// https://leetcode.com/problems/climbing-stairs/description/
+class Solution {
+    func climbStairs(_ n: Int) -> Int {
+        if n == 0 || n == 1 {
+            return 1
+        }
+
+        var prev1 = 1
+        var prev2 = 1
+
+        for _ in 2...n {
+            let temp = prev1
+            prev1 = prev2 + temp
+            prev2 = temp
+        }
+
+        return prev1
+    }
+}
+/*
+class Solution {
+    func climbStairs(_ n: Int) -> Int {
+        if n == 0 || n == 1 {
+            return 1
+        }
+
+        var dp = [Int](repeating: 0, count: n + 1)
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in 2...n {
+            dp[i] = dp[i - 1] + dp[i - 2]
+        }
+
+        return dp[n]
+    }
+}
+*/
