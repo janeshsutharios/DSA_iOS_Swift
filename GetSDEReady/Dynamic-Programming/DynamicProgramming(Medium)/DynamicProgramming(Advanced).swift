b798b82f936a4daaf9309e471f8d74cc80b21df4
Janesh suthar <janesh.stack@gmail.com>
2025-07-25 18:05:58 +0530

    Update DynamicProgramming(Advanced).swift

--- GetSDEReady/Dynamic-Programming/DynamicProgramming(Medium)/DynamicProgramming(Advanced).swift
+++ GetSDEReady/Dynamic-Programming/DynamicProgramming(Medium)/DynamicProgramming(Advanced).swift
@@ -8,3 +8,35 @@ class Solution {
       //(n & 1) == 0 means: check if n is even.
     }
 }
+// https://getsdeready.com/courses/dsa/lesson/n-th-tribonacci-number/
+// https://leetcode.com/problems/n-th-tribonacci-number/description/
+class Solution {
+    func tribonacci(_ n: Int) -> Int {
+        if n == 0 { return 0 }
+        if n == 1 || n == 2 { return 1 }
+
+        var dp = [0, 1, 1]
+        for i in 3...n {
+            dp[i % 3] = dp[0] + dp[1] + dp[2]
+        }
+        return dp[n % 3]
+    }
+}
+
+/*
+class Solution {
+    func tribonacci(_ n: Int) -> Int {
+        guard n > 0 else { return 0 }
+        guard n > 2 else { return 1 }
+        
+        var result = 0
+        var (a, b, c) = (0, 1, 1)
+        for _ in 3...n {
+            result = c + b + a
+            a = b
+            b = c
+            c = result
+        }
+        return result
+    }
+}*/
