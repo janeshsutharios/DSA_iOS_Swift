3b5e6468fdfc3a3d72fa55164fc958e10b75bfce
Janesh suthar <janesh.stack@gmail.com>
2025-07-25 17:23:15 +0530

    Create DynamicProgramming(Advanced).swift

---
+++ GetSDEReady/Dynamic-Programming/DynamicProgramming(Medium)/DynamicProgramming(Advanced).swift
@@ -0,0 +1,10 @@
+
+// https://getsdeready.com/courses/dsa/lesson/divisor-game/
+// https://leetcode.com/problems/divisor-game/description/
+class Solution {
+    func divisorGame(_ n: Int) -> Bool {
+        //n % 2 == 0
+         (n & 1) == 0  // The binary & (bitwise AND) checks the least significant bit (LSB) of n.
+      //(n & 1) == 0 means: check if n is even.
+    }
+}
