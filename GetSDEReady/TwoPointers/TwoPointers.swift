7db61a25b3e1ac0ed038e73a54851915cd80a5e9
Janesh suthar <janesh.stack@gmail.com>
2025-08-04 19:35:27 +0530

    Update TwoPointers.swift

--- GetSDEReady/TwoPointers/TwoPointers.swift
+++ GetSDEReady/TwoPointers/TwoPointers.swift
@@ -35,3 +35,21 @@ class Solution {
     }
 }
 */
+
+// https://getsdeready.com/courses/dsa/lesson/check-if-n-and-its-double-exist/
+// https://leetcode.com/problems/check-if-n-and-its-double-exist/description/
+class Solution {
+    func checkIfExist(_ arr: [Int]) -> Bool {
+        var dict: [Int: Bool] = [:]  // stores seen numbers
+
+        for num in arr {
+            // check if current number has a double or half in the map
+            if dict[2 * num] == true || (num % 2 == 0 && dict[num / 2] == true) {
+                return true
+            }
+            dict[num] = true  // mark this number as seen
+        }
+
+        return false
+    }
+}
