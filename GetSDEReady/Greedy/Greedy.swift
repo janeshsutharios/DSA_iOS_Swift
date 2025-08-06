53910d3ed4d7422f5257c4636899172f3d5cbe31
Janesh suthar <janesh.stack@gmail.com>
2025-08-06 09:56:39 +0530

    Update Greedy.swift

--- GetSDEReady/Greedy/Greedy.swift
+++ GetSDEReady/Greedy/Greedy.swift
@@ -15,3 +15,27 @@ class Solution {
         return 0
     }
 }
+// https://getsdeready.com/courses/dsa/lesson/longest-palindrome/
+// https://leetcode.com/problems/longest-palindrome/description/
+class Solution {
+    func longestPalindrome(_ s: String) -> Int {
+        var freq: [Character: Int] = [:]
+        var length = 0
+        // Rule 1: Use all even counts completely c = 4 → use all 4 → 2 on left, 2 on right
+        for char in s {
+            freq[char, default: 0] += 1
+            if freq[char]! % 2 == 0 {
+                length += 2
+            } 
+        }
+     //Odd But we can use one of them as the center character in the palindrome.
+     // Example: bccaccb
+        for value in freq.values {
+            if value % 2 != 0 {
+                length += 1
+                break
+            }
+        }
+        return length
+    }
+}
